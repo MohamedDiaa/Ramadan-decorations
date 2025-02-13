ad7dc56c5e090724fb7f496fda17e3003044464d
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-02-13 20:44:00 +0200

    add marks along the curve

--- point.js
+++ point.js
@@ -1,44 +1,51 @@
-// let p0x = 20;
-// let p0y = 20;
-
-// let p1x = 380;
-// let p1y = 400;
-
-// let p2x = 0
-// let p2y = 0
-
-// let p3x = 780;
-// let p3y = 20;
-
-
-//        ctx.moveTo(20, 20);
-
-//   ctx.quadraticCurveTo((800 - 40) / 2, 0.8 * 500, 800 - 20, 20);
-
-
-// function quadratic(p0, p1, p2, t){ 
-//     return (Math.pow((1 - t), 2) * p0) +  (2 * (1-t) * t * p1) + (Math.pow(t,2) * p2)
-// }
-
-// let t1 = 0.8
-// let r1x = quadratic(p0x,p1x,p2x,p3x,t1);
-// let r1y = quadratic(p0y,p1y,p2y,p3y,t1);
-// console.log(r1x, r1y)
-
-
-
-const P0 = { x: 20, y: 20 };
-const P1 = { x: 380, y: 400 };
-const P2 = { x: 780, y: 20 };
+// const P0 = { x: 20, y: 20 };
+// const P1 = { x: 380, y: 400 };
+// const P2 = { x: 780, y: 20 };
 
 function getBezierPoint(t, P0, P1, P2) {
-    const x = (1 - t) ** 2 * P0.x + 2 * (1 - t) * t * P1.x + t ** 2 * P2.x;
-    const y = (1 - t) ** 2 * P0.y + 2 * (1 - t) * t * P1.y + t ** 2 * P2.y;
-    return { x, y };
+  const x = (1 - t) ** 2 * P0.x + 2 * (1 - t) * t * P1.x + t ** 2 * P2.x;
+  const y = (1 - t) ** 2 * P0.y + 2 * (1 - t) * t * P1.y + t ** 2 * P2.y;
+  return { x, y };
 }
 
-const points = [];
-for (let t = 0; t <= 1; t += 0.1) {
-    points.push(getBezierPoint(t, P0, P1, P2));
+// -------- Dom code -----------
+
+const canvas = document.getElementById("myCanvas");
+resizeCanvas();
+
+// resize the canvas to fill browser window dynamically
+window.addEventListener("resize", resizeCanvas, false);
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  drawStuff();
 }
-console.log(points);
+
+function drawStuff() {
+  // do your drawing stuff here
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  console.log(width, height);
+
+  let p0 = { x: 0, y: 0 };
+  let p1 = { x: width / 2, y: 0.8 * height };
+  let p2 = { x: width, y: 0 };
+
+  const ctx = canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.moveTo(p0.x, p0.y);
+  ctx.quadraticCurveTo(p1.x, p1.y, p2.x, p2.y);
+  ctx.stroke();
+
+  const points = [];
+  for (let t = 0; t <= 1; t += 0.1) {
+    points.push(getBezierPoint(t, p0, p1, p2));
+  }
+
+  const div = document.getElementById("lamps");
+   
+  let items = points.map(pt => `<h1 style="position: absolute;color: red;left: ${pt.x}px;top: ${pt.y}px">X</h1>`);
+  div.innerHTML = items;
+}
